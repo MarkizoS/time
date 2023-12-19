d756d43a798773961c369fd78b7c1d25e283afe3
Mark <markiz89535@gmail.com>
2023-12-19 15:51:39 +0300

    add a stopinteval

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-(function(){
-    let sec = '00', min = '00', hour = '00';
+function time(){
+    let sec = '58', min = '59', hour = '00'; 
     
-setInterval(function(){
-  sec = +sec +1;
-if( sec < 10 ) {
-    sec = '0' + sec; 
+    function tick(){
+        sec = +sec +1;
+        if( sec < 10 ) {
+            sec = '0' + sec; 
+        }
+        if( sec == 60 ) {
+            sec = '00';
+            min = +min + 1;
+            if( min < 10 ) { 
+                min = '0' + min; 
+            }
+            if( min == 60 ) {
+                min = '00';
+                hour = +hour + 1;
+                if( hour < 10 ) {
+                    hour = '0' + hour; 
+                }
+            }
+        }
+        console.log(`sec: ${sec}, min: ${min}, hour: ${hour}`);
     }
-if( sec == 60 ) {
-    sec = '00';
-    min = +min + 1;
-    if( min < 10 ) { 
-        min = '0' + min; 
-        }
-    if( min == 60 ) {
-        min = '00';
-        hour = +hour + 1;
-    if( hour < 10 ) {
-        hour = '0' + hour; 
-        }
-    }}},1000);
-});
+    
+    let timerId = setInterval(tick ,1000);
+
+    function stop(){
+        clearInterval(timerId);
+    }
+    
+}
+
 
