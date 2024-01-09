b409de2472aaa3004d8a89b85f4e9570cd2610fc
Mark <markiz89535@gmail.com>
2024-01-09 19:31:31 +0300

    wrote startimer stoptimer circle

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-(function(){
-    let sec = '00', min = '00', hour = '00';
+let GlobalTime = 0 
+let timerId 
+let CircleArr = [];
+let CircleTime = 0
+let CircleNum = 0 
+
+function StartTimer(){
+    let sec = '00', min = '00', hour = '00'; 
     
-setInterval(function(){
-  sec = +sec +1;
-if( sec < 10 ) {
-    sec = '0' + sec; 
+    let TimeStamp = 0 
+
+    function tick(){
+        sec = +sec +1;
+        if( sec < 10 ) {
+            sec = '0' + sec; 
+        }
+        if( sec == 60 ) {
+            sec = '00';
+            min = +min + 1;
+            if( min < 10 ) { 
+                min = '0' + min; 
+            }
+            if( min == 60 ) {
+                min = '00';
+                hour = +hour + 1;
+                if( hour < 10 ) {
+                    hour = '0' + hour; 
+                }
+            }
+        }
+        console.log(`sec: ${sec}, min: ${min}, hour: ${hour}`);
+        
+        GlobalTime += 1
+        CircleTime += 1 
     }
-if( sec == 60 ) {
-    sec = '00';
-    min = +min + 1;
-    if( min < 10 ) { 
-        min = '0' + min; 
-        }
-    if( min == 60 ) {
-        min = '00';
-        hour = +hour + 1;
-    if( hour < 10 ) {
-        hour = '0' + hour; 
-        }
-    }}},1000);
-});
+
+    timerId = setInterval(tick ,1000);
+    
+}
+
+function StopTimer(){
+    console.log(GlobalTime);
+    clearInterval(timerId);
+}
+
+function Circle(){
+     CircleNum += 1; 
+     let box = {
+        Time: CircleTime,
+        Num: CircleNum
+     };
+    CircleArr.push(box);
+    CircleTime = 0
+}
+
+function GetCircleArr(){
+    console.log(CircleArr)
+}
+
 
