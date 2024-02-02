f7b4bb6cdde55779799035d7c97654327da66fdc
Mark <markiz89535@gmail.com>
2024-02-02 16:37:36 +0300

    last moves

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     let GlobalTime = 0
     let timerId = null
     let CircleArr = [];
+    // {Num:1, Time:1}, {Num:2, Time: 6} 
     let CircleTime = 0
-    let CircleNum = 0
+    let CircleNum = 1
     let osth = 0 
     let hasStart = false
-
     let buttonRefresh = document.getElementById("Refresh");
     let buttonCircle = document.getElementById("Circle");
     let buttonStart = document.getElementById("Start");
     let button = document.getElementById("Stop");
     let NumString = document.getElementById("NumString")
-    let table = document.getElementById("NumTable")
-    console.log(button)
+    let Table = document.getElementById("NumTable")
 
     buttonStart.addEventListener('click',StartTimer );
     button.addEventListener('click',StopTimer );
@@ -24,32 +23,36 @@ document.addEventListener('DOMContentLoaded', function(){
     button.style.display = "none";
     buttonRefresh.style.display = "none";
 
+    let sec = GlobalTime
+    let min = '00' 
+    let hour = '00'
+    
     function StartTimer(){
+
         buttonRefresh.style.display = "none";
         buttonStart.style.display = "none";
         buttonCircle.style.display = "";
         button.style.display = "";
-        let sec = GlobalTime
-        min = '00'
-        hour = '00';
         
         if (hasStart) {
             return;
           }
 
         hasStart = true;
-
+        
         function tick(){
+
+            let sec = GlobalTime
+
             if(GlobalTime >= 60){
                 sec = GlobalTime % 60
-                ost = ((GlobalTime % 60)/60)  
+                ost = (GlobalTime % 60)/60 
                 min = (GlobalTime / 60) - ost;
-                min = +min - 60
             }
             if(min >= 60){
                 osth = (GlobalTime % 3600)/3600  
                 hour = (GlobalTime / 3600) - osth
-                min = +min - 60 
+                min = +min - 60
             }
             sec = +sec +1;
             if( sec < 10 ) {
@@ -65,25 +68,21 @@ document.addEventListener('DOMContentLoaded', function(){
                     min = '00';
                     hour = +hour + 1;
                     if( hour < 10 ) {
-                        hour = '0' + hour; 
+                        hour = '0' + hour;
                     }
                 }
-               
             }
             console.log(`sec: ${sec}, min: ${Math.ceil(min)}, hour: ${Math.ceil(hour)}`);
-            
-            document.getElementById("NumString").innerHTML = `${Math.ceil(hour)}:${Math.ceil(min)},${sec}`;
 
+            document.getElementById("NumString").innerHTML = `${Math.ceil(hour)}:${Math.ceil(min)},${sec}`;
+            
             GlobalTime += 1;
             CircleTime += 1;
 
             console.log(`GlobalTime:${GlobalTime}`)
-            
         }
         console.log(sec)
         timerId = setInterval(tick ,1000);
-        
-        
     }
 
     function StopTimer(){
@@ -97,31 +96,71 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     function Circle(){
+        
+        // console.log(CircleArr[0])
+        // console.log(CircleArr[0].Time)
+
+        // console.log(user.Time)
+
+        let sum = 0;
+        for (let i = 0; i < CircleArr.length; i++){
+            sum += +CircleArr[i].Time;
+            console.log('массив:', CircleArr[i].Time)
+            // CircleTime = sum - CircleArr[i].Time
+        }
+
+        console.log('Сумма элементов массива: ' + sum);
+  
+        
+
+        // CircleArr.forEach(function(item, i, arr) {
+        //     console.log( i + ": " + item + " (массив:" + arr + ")" );
+        // });
+        
+        // let res = CircleArr.reduce(function(sum, elem) {
+        //     if ( elem <= 2 ) { 
+        //         return +sum + +elem;
+        //     } else {
+        //         return +sum;
+        //     }
+        // });
+        
+        // console.log('сумма первых двух:' +res);
+        
+        let box = {
+            Time: CircleTime,
+            Num: CircleNum
+        };
+        
+        CircleArr.push(box);
+        
+        let newRow = Table.insertRow(0)
+        let newCellCircleName = newRow.insertCell(0)
+        let newCellClear = newRow.insertCell(0)
+        let newCellCircleTime = newRow.insertCell(0)
+        let newTextCircleName = document.createTextNode(`${CircleTime} Секунд`)
+        let newTextClear = document.createTextNode(``)
+        let newTextCircleTime = document.createTextNode(`Круг ${CircleNum}`)
+        newCellCircleName.appendChild(newTextCircleName)
+        newCellCircleTime.appendChild(newTextCircleTime)
+        newCellClear.appendChild(newTextClear)
+        
+
         console.log(CircleArr)
         document.getElementById("Circle").innerHTML
         hasStart = true
         CircleNum += 1; 
-        let box = {
-        Time: CircleTime,
-        Num: CircleNum
-        };
-        CircleTime = GlobalTime
-        CircleArr.push(box);
-        document.getElementById("numOfcircle").innerHTML = `Круг ${CircleNum}`
-        document.getElementById("circleStr").innerHTML = `${CircleTime} Секунд`
     }
-    
 
     function Refresh(){
         clearInterval(timerId)
         document.getElementById("NumString").innerHTML = `0:0,00`
+        CircleArr.splice(0, CircleArr.length);
         GlobalTime = 0;
         CircleTime = 0;
         CircleNum = 0;
-        CircleArr = []
         hasStart = false 
-        document.getElementById("numOfcircle").innerHTML = ``
-        document.getElementById("circleStr").innerHTML = ``
+        document.getElementById("NumTable").innerHTML = `` 
     }
     
 });
