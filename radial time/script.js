2d0ec66c6a58f8c63b30e701db3cb90bd0bd6bc7
Mark <markiz89535@gmail.com>
2024-02-20 15:33:11 +0300

    doned timer

--- radial time/script.js
+++ radial time/script.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function(){
     let Table = document.getElementById("NumTable")
 
 
-
     buttonStart.addEventListener('click',StartTimer,);
     button.addEventListener('click',StopTimer );
     buttonCircle.addEventListener('click',Circle );
@@ -23,93 +22,73 @@ document.addEventListener('DOMContentLoaded', function(){
 
     button.style.display = "none";
     buttonRefresh.style.display = "none";
-    
+
+    let millisec = '00' 
     let sec = GlobalTime
     let min = '00' 
     let hour = '00'
-    let minutes = min
-    let seconds = sec
     
-    let  hands = [
-        {   hand: 'miniStrelka',
-            angle: (minutes * 6)
-        },
-        {   hand: 'bigStrelka',
-            angle: (seconds * 6)
-        }
-    ]
-    
-
     function StartTimer(){
+    
+        sec = GlobalTime
 
-        
-        buttonRefresh.style.display = "none";
-          buttonStart.style.display = "none";
-          buttonCircle.style.display = "";
-          button.style.display = "";
+              if(GlobalTime >= 60){
+                  sec = GlobalTime % 60
+                  ost = (GlobalTime % 60)/60 
+                  min = (GlobalTime / 60) - ost;
+                }
+              if(min >= 60){
+                  osth = (GlobalTime % 3600)/3600  
+                  hour = (GlobalTime / 3600) - osth
+                  min = +min - 60
+                }
+              if( sec < 10 ) {
+                  sec = '0' + sec; 
+                }
+            
+            
+            buttonRefresh.style.display = "none";
+            buttonStart.style.display = "none";
+            buttonCircle.style.display = "";
+            button.style.display = "";
           
-          
-          if (hasStart) {
-              return;
+            if (hasStart) {
+                return;
             }
             
             hasStart = true;
             
-            bigStrelka.style.animation = "rotate 60s infinite linear";
-            
-            for (var j = 0; j < hands.length; j++) {
-                var elements = document.querySelectorAll('.' + hands[j].hand);
-                for (var k = 0; k < elements.length; k++) {
-                    elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
-                    elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
-                    if (hands[j].hand === 'minutes') {
-                      elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
-                    }
-                }
-            }
-            
+            biggestStrelka.style.animation = "rotate 60s infinite linear";
+            littlestStrelka.style.animation = "rotate 1800s infinite linear";
+
+
             function tick(){
-            let sec = GlobalTime
 
-            if(GlobalTime >= 60){
-                sec = GlobalTime % 60
-                ost = (GlobalTime % 60)/60 
-                min = (GlobalTime / 60) - ost;
-            }
-            if(min >= 60){
-                osth = (GlobalTime % 3600)/3600  
-                hour = (GlobalTime / 3600) - osth
-                min = +min - 60
-            }
-            sec = +sec +1;
-            if( sec < 10 ) {
-                sec = '0' + sec; 
-            }
-            if( sec == 60 ) {
-                sec = '00';
-                min = +min + 1;
-                if( min < 10 ) { 
-                    min = '0' + min; 
+                millisec = +millisec + 10;
+
+                if (millisec >= 1000) {
+                    millisec = 0;
+                    sec = +sec + 1;
                 }
-                if( min == 60 ) {
-                    min = '00';
-                    hour = +hour + 1;
-                    if( hour < 10 ) {
-                        hour = '0' + hour;
+                if( sec == 60 ) {
+                    sec = '00';
+                    min = +min + 1;
+                    if( min < 10 ) { 
+                        min = '0' + min; 
                     }
-                }
+                    if( min == 60 ) {
+                        min = '00';
+                        hour = +hour + 1;
+                        if( hour < 10 ) {
+                            hour = '0' + hour;
+                        }
+                    }
+                  }   
+
+            document.getElementById("NumString").innerHTML = `${Math.ceil(hour)}:${Math.ceil(min)},${Math.ceil(sec)},${Math.ceil(millisec)}`;
             }
-            
-            document.getElementById("NumString").innerHTML = `${Math.ceil(hour)}:${Math.ceil(min)},${sec}`;
-            
-            GlobalTime += 1;
-            CircleTime += 1;
 
-        }
-        
-        timerId = setInterval(tick ,1000);
-
-      
+        timerId = setInterval(tick ,10);
     }
     
     function StopTimer(){
@@ -119,16 +98,19 @@ document.addEventListener('DOMContentLoaded', function(){
         buttonRefresh.style.display = "";
         clearInterval(timerId);
         hasStart = false
+        littlestStrelka.style.animationPlayState = 'paused';
+        biggestStrelka.style.animationPlayState = 'paused';
     }
 
     function Circle(){
-    
+
         let sum = 0;
+
         for (let i = 0; i < CircleArr.length; i++){
             sum += +CircleArr[i].Time;
             
         }
-     
+        
         let box = {
             Time: CircleTime - sum,
             Num: CircleNum,
@@ -155,15 +137,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function Refresh(){
         clearInterval(timerId)
-        document.getElementById("NumString").innerHTML = `0:0,00`
+        document.getElementById("NumString").innerHTML = `0:0,00,000`
         CircleArr.splice(0, CircleArr.length);
         GlobalTime = 0;
         CircleTime = 0;
         CircleNum = 0;
         hasStart = false 
         document.getElementById("NumTable").innerHTML = `` 
+        biggestStrelka.style.animation = "";
+        littlestStrelka.style.animation = "";
     }
-
-    
-    
 })
